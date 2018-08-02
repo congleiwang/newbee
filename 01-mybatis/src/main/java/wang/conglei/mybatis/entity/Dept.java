d3d1c27eba923a5cc963330567dfd82b6bf307ac
wangcl <wangcl@yanxintec.com>
2018-08-02 11:07:23 +0800

    add 创建项目，将mybatis项目整合进来

---
+++ 01-mybatis/src/main/java/wang/conglei/mybatis/entity/Dept.java
@@ -0,0 +1,54 @@
+package wang.conglei.mybatis.entity;
+
+/**
+ * @program: mybatis
+ * @description: 实体类，部门
+ * @author: conglei.Wang
+ * @create: 2018-07-16 22:57
+ **/
+public class Dept {
+    private Integer id;
+    private String deptName;
+    private String loc;
+
+    public Dept() {
+    }
+
+    public Dept( String deptName, String loc) {
+        this.deptName = deptName;
+        this.loc = loc;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDeptName() {
+        return deptName;
+    }
+
+    public void setDeptName(String deptName) {
+        this.deptName = deptName;
+    }
+
+    public String getLoc() {
+        return loc;
+    }
+
+    public void setLoc(String loc) {
+        this.loc = loc;
+    }
+
+    @Override
+    public String toString() {
+        return "Dept{" +
+                "id=" + id +
+                ", deptName='" + deptName + '\'' +
+                ", loc='" + loc + '\'' +
+                '}';
+    }
+}
